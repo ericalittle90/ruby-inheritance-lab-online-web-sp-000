b854ca1590459b5ce9353170262ae12d06996816
Erica Little <eteney90@gmail.com>
2021-04-12 21:43:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,16 @@
 class Student < User
 
+  attr_accessor :knowledge 
 
+  def initialize(knowledge)
+      @knowlegde = [] 
+  end 
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  def knowledge
+    @knowledge
+  end
 
 end
